0dcaa851b57b5d1478dce05371bbd3d5b10d7936
kishore <skill2automate@gmail.com>
2025-06-19 08:00:15 +0530

    added ne test cases

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,7 +8,16 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('get started link 1', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+test('get started link 2', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
